7415bec7b0089ce551aef7ffe72d9bdf35b2b8b3
khalilii <khalili.yassinee@gmail.com>
2025-02-05 13:38:49 +0100

    Effacer tout le contenu du dépôt

--- test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-describe('Test Server', function () {
-  it('devrait renvoyer "Server running on port 3000"', function () {
-    const response = 'Server running on port 3000';
-    assert.strictEqual(response, 'Server running on port 3000');
-  });
-});
